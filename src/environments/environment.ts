2e3bf3758814909e67493d2d16a6735fb9abbebe
maxmilin83 <120525774+maxmilin83@users.noreply.github.com>
2025-05-01 20:20:43 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  openAiApiKey: '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'
+  openAiApiKey: 'YOUR_API_KEY'
 };
 
 /*
